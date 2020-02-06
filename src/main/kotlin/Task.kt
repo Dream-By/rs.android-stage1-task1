e4b3f83ebe136644c7ce6f445fc49570cd3976b5
Artem Bagritsevich <artem.bagritsevich@gmail.com>
2020-02-06 20:30:18 +0300

    Update Task.kt

--- src/main/kotlin/Task.kt
+++ src/main/kotlin/Task.kt
@@ -1,10 +1,6 @@
 class Task {
 
     fun isAndroid(string: String): Boolean {
-        return string == ANDROID_STRING
-    }
-
-    private companion object {
-        private const val ANDROID_STRING = "Android"
+        throw NotImplementedError("Not implemented")
     }
 }
