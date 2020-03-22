48662e32b8e54554fb4cf0c454eb872c8b9647ee
dzmitry.dzianisau@gmail.com <Dream-By>
2020-03-22 09:36:35 +0300

    second task

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,6 +4,36 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var max = 0
+        var min = 100000000
+        var sum = 0
+        var count = 0
+
+
+        val result: IntArray
+
+        if (input != null) {
+
+            while (count < input.size) {
+
+                for (i in input.indices) {
+
+                    if (i != count) {
+                        sum += input[i]
+                    }
+                }
+
+                when {
+                    sum >= max -> max = sum
+                    sum <= min -> min = sum
+                }
+                sum = 0
+                count++
+            }
+        } else {
+            throw NotImplementedError("Not implemented")
+        }
+        result = intArrayOf(min, max)
+        return result
     }
 }
