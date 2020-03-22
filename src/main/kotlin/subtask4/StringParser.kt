24880fb0454884dae7e18d71fa1341f2f421ef72
dzmitry.dzianisau@gmail.com <Dream-By>
2020-03-22 11:55:42 +0300

    fourth task

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -4,6 +4,37 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val arrayresult: MutableList<String> = arrayListOf()
+
+        if (inputString != null) {
+            for (i in inputString.indices) {
+                when {
+                    inputString[i] == '<' -> arrayresult.add(getSubstring(inputString.substring(i), '<', '>'))
+                    inputString[i] == '(' -> arrayresult.add(getSubstring(inputString.substring(i), '(', ')'))
+                    inputString[i] == '[' -> arrayresult.add(getSubstring(inputString.substring(i), '[', ']'))
+                }
+            }
+        } else {
+            throw NotImplementedError("Not implemented")
+        }
+
+        return arrayresult.toTypedArray()
+    }
+
+    fun getSubstring(string: String, charOpen: Char, charEnd: Char): String {
+        var count = 0
+        var result = ""
+
+        for (i in 1 until string.length) {
+            if (string[i] == charOpen) {
+                count++
+            } else if (string[i] == charEnd && count != 0) {
+                count--
+            } else if (string[i] == charEnd && count == 0) {
+                result = string.substring(1, i)
+                break
+            }
+        }
+        return result
     }
 }
